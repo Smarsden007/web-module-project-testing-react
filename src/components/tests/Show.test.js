249c18e3f0e6d53779fe7fac9a5192cdd11ea939
Smarsden007 <seannmarsden@gmail.com>
2022-04-25 20:48:59 -0700

    Show Test Built out

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -2,13 +2,49 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
+import userEvent from '@testing-library/user-event';
+
+const testShow ={
+    name: "test show",
+    summary: "test summary",
+    season: [
+        {
+            id: 0,
+            name:"Season 1",
+            episode:[]
+        },
+        {
+            id:1,
+            name: "Season 2",
+            episode: []
+        }
+    ]
+};
 
 test('renders without errors', () => { });
+    render(<Show show={testShow} selectedSeason={"none"}/> )
 
-test('renders Loading component when prop show is null', () => { });
 
-test('renders same number of options seasons are passed in', () => { });
+test('renders Loading component when prop show is null', () => { 
+    render(<Show show={null} />);
+    const loading = screen.queryByTestId('loading-container');
+    expect(loading).toBeInTheDocument();
 
-test('handleSelect is called when an season is selected', () => { });
+});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+test('renders same number of options seasons are passed in', () => { 
+    render(<Show show={testShow} selectedSeason={"none"}/>);
+    const seasonOptions = screen.queryAllByTestId('season-option');
+    expect(seasonOptions).toHaveLength(2);
+
+});
+
+test('handleSelect is called when an season is selected', () => { 
+    render(<Show show={testShow} selectedSeason={"none"}/>);
+    const select = screen.querygetByLabelText(/Select a Season/i);
+    userEvent.selectOptions(select, ['1']);
+});
+
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => { 
+    render(<Show show={testShow}/>)
+});
